8b62cd4910b1f15ee508ccd4aa6c10317d2b151c
pmq <piao_minqing@nhn-st.com>
2020-01-03 17:57:38 +0800

    20200103

--- vue-study-web16/src/plugins/my-vuex/store.js
+++ vue-study-web16/src/plugins/my-vuex/store.js
@@ -1,15 +1,31 @@
 import { _Vue } from "./install";
 export default class Store {
   constructor(options){
+    this._mutations = options.mutations || {}
+    this._actions = options.actions || {}
     // state 响应化处理
     this.state = new _Vue({
       data:options.state
     })
-  }
-  commit(){
 
+    // 异步执行会丢失 this (commit,dispatch)
+    this.commit = this.commit.bind(this)
+    this.dispatch = this.dispatch.bind(this)
   }
-  dispatch(){
-
+  commit(type, payload){
+    const mutation = this._mutations[type]
+    if(!mutation){
+      console.error(`unknown mutation type:${type}`);
+      return
+    }
+    mutation(this.state, payload)
+  }
+  dispatch(type, payload){
+    const action = this._actions[type]
+    if(!action){
+      console.error(`unknown mutation type:${type}`);
+      return
+    }
+    action(this, payload)
   }
 }
