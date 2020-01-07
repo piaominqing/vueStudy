d9e445ff5488f5a430fe9c50d85abdf7502df5d9
pmq <piao_minqing@nhn-st.com>
2020-01-07 15:58:09 +0800

    20200102-task3

--- vue-study-web16/src/main.js
+++ vue-study-web16/src/main.js
@@ -5,6 +5,30 @@ import create from './utils/create'
 import extendsCreate from './utils/extendsCreate'
 import router from './router'
 import store from './store'
+import {Observer, Watcher} from './utils/reactive'
+
+let data = {
+  name: 'cjg',
+  obj: {
+    name: 'zht',
+  },
+};
+
+new Observer(data);
+// 监听data对象的name属性，当data.name发现变化的时候，触发cb函数
+new Watcher(data, 'name', (oldValue, newValue) => {
+  console.log(oldValue, newValue);
+})
+
+// data.name = 'zht';
+
+// // 监听data对象的obj.name属性，当data.obj.name发现变化的时候，触发cb函数
+// new Watcher(data, 'obj.name', (oldValue, newValue) => {
+//   console.log(oldValue, newValue);
+// })
+
+// data.obj.name = 'cwc';
+// data.obj.name = 'dmh';
 
 Vue.config.productionTip = false
 // 事件总线
