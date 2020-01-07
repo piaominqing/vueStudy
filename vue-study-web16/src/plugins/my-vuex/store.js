e489e0e64ab65ea434f2e85382258e1516968a3e
pmq <piao_minqing@nhn-st.com>
2020-01-07 10:10:43 +0800

    20200102-task2

--- vue-study-web16/src/plugins/my-vuex/store.js
+++ vue-study-web16/src/plugins/my-vuex/store.js
@@ -3,14 +3,28 @@ export default class Store {
   constructor(options) {
     this._mutations = options.mutations || {}
     this._actions = options.actions || {}
+    this._wrappedGetters = options.getters || {}
+    let computed = {}
+    const store = this
+    store.getters = {}
+
+    Object.keys(this._wrappedGetters).forEach((key)=>{
+      computed[key] = function () {
+        return store._wrappedGetters[key](store.state)
+      }
+
+      Object.defineProperty(store.getters, key, {
+        get: () => store._vm[key]
+      })
+    })
     // state 响应化处理
     this._vm = new _Vue({
       data: {
         // 俩个$$ vue 不做处理
         $$state: options.state
-      }
+      },
+      computed
     });
-
     // 异步执行会丢失 this (commit,dispatch)
     this.commit = this.commit.bind(this)
     this.dispatch = this.dispatch.bind(this)
--- vue-study-web16/src/views/Home.vue
+++ vue-study-web16/src/views/Home.vue
@@ -8,7 +8,7 @@
     <!-- <my-form></my-form> -->
     <p @click="$store.commit('add')">counter:{{$store.state.counter}}</p>
     <p @click="$store.dispatch('add')">async counter:{{$store.state.counter}}</p>
-    <!-- <p>double counter:{{$store.getters.doubleCounter}}</p> -->
+    <p>double counter:{{$store.getters.doubleCounter}}</p>
   </div>
 </template>
 <script>
