21ed26219c927925f825361454451da75d7fce22
piao-minqing <piao_minqing@nhn-st.com>
2020-01-12 17:29:32 +0800

    20200112

--- vue-sound-code-study/src/platforms/web/entry-runtime-with-compiler.js
+++ vue-sound-code-study/src/platforms/web/entry-runtime-with-compiler.js
@@ -9,59 +9,56 @@ import { query } from './util/index'
 import { compileToFunctions } from './compiler/index'
 import { shouldDecodeNewlines, shouldDecodeNewlinesForHref } from './util/compat'
 
+// 创建根据id获取innerHTML的方法
+// cached 将获取结果存入cache
+// query document.querySelector
 const idToTemplate = cached(id => {
   const el = query(id)
   return el && el.innerHTML
 })
 
+// 存一下Vue的原型方法$mount
 const mount = Vue.prototype.$mount
+// 重写Vue的原型方法$mount
 Vue.prototype.$mount = function (
   el?: string | Element,
   hydrating?: boolean
 ): Component {
   el = el && query(el)
 
-  /* istanbul ignore if */
-  if (el === document.body || el === document.documentElement) {
-    process.env.NODE_ENV !== 'production' && warn(
-      `Do not mount Vue to <html> or <body> - mount to normal elements instead.`
-    )
-    return this
-  }
-
   const options = this.$options
   // resolve template/el and convert to render function
+  // 当Vue构造函数中不存在不存在render函数时
   if (!options.render) {
     let template = options.template
+    // 当template存在时
     if (template) {
+      // 当template为string时
       if (typeof template === 'string') {
+        // 当template为id选择器时
         if (template.charAt(0) === '#') {
+          // 根据id获取template
           template = idToTemplate(template)
-          /* istanbul ignore if */
-          if (process.env.NODE_ENV !== 'production' && !template) {
-            warn(
-              `Template element not found or is empty: ${options.template}`,
-              this
-            )
-          }
         }
+      // 当template存在nodeType时 
       } else if (template.nodeType) {
+        // 直接取innerHTML为tempalate
         template = template.innerHTML
+      // 否则 报错
       } else {
         if (process.env.NODE_ENV !== 'production') {
           warn('invalid template option:' + template, this)
         }
         return this
       }
+    // 当el存在时
     } else if (el) {
+      // 取outerHTML为template
       template = getOuterHTML(el)
     }
+    // 以上获取到template时
     if (template) {
-      /* istanbul ignore if */
-      if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-        mark('compile')
-      }
-
+      // 待深入了解
       const { render, staticRenderFns } = compileToFunctions(template, {
         outputSourceRange: process.env.NODE_ENV !== 'production',
         shouldDecodeNewlines,
@@ -72,13 +69,9 @@ Vue.prototype.$mount = function (
       options.render = render
       options.staticRenderFns = staticRenderFns
 
-      /* istanbul ignore if */
-      if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-        mark('compile end')
-        measure(`vue ${this._name} compile`, 'compile', 'compile end')
-      }
     }
   }
+  // 执行原Vue $mount 方法并返回
   return mount.call(this, el, hydrating)
 }
 
